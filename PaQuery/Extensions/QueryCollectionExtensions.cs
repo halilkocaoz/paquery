51bb38752ba79ddfe7fa2d3b742b889b4681e7dc
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-03-06 00:35:31 +0300

    Add library project references and QueryCollectionExtensions file

---
+++ PaQuery/Extensions/QueryCollectionExtensions.cs
@@ -0,0 +1,10 @@
+namespace PaQuery//.Extensions
+{
+    public static class QueryCollectionExtensions
+    {
+        public static PageUrl GetUrls(this IQueryCollection queryCollection)
+        {
+            return null;
+        }
+    }
+}
