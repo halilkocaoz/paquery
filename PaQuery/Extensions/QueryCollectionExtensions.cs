3a06391eef55cdc16bb9fe244e27a22134692c94
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-03-06 01:04:44 +0300

    Add toStringQueriesWithoutPageQueryKey

--- PaQuery/Extensions/QueryCollectionExtensions.cs
+++ PaQuery/Extensions/QueryCollectionExtensions.cs
@@ -1,10 +1,35 @@
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using PaQuery.Models;
+
 namespace PaQuery//.Extensions
 {
     public static class QueryCollectionExtensions
     {
-        public static PageUrl GetUrls(this IQueryCollection queryCollection)
+        public static string toStringQueriesWithoutPageQueryKey(this IQueryCollection queryCollection, string pageQueryKey)
         {
-            return null;
+            var queryArray = queryCollection.ToArray();
+            var areThereAnyQueryOtherThanPageQuery = queryArray.Length > 1 || queryArray.Length == 1 && pageQueryKey != queryArray[0].Key;
+            if (areThereAnyQueryOtherThanPageQuery is false) return null;
+            StringBuilder queries = new StringBuilder();
+            foreach (var query in queryArray)
+            {
+                if (pageQueryKey == query.Key)
+                    continue;
+                queries.Append(query.Key + "=" + query.Value[0].Replace(" ", "%20") + "&");
+            }
+            return "?" + queries;
+        }
+        public static PageUrl GetUrls(
+            this IQueryCollection queryCollection,
+            string hostPath,
+            int totalPageCount,
+            int currentPage,
+            string pageQueryKey = "page")
+        {
+            string next = null, prev = null;
+            return new PageUrl(next, prev);
         }
     }
 }
