6e9755c9adeea27c14a75daa1c96421eab7551ff
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-06-27 20:39:37 +0300

    Init test project

---
+++ PaQuery.Tests/PageInfoTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace PaQuery.Tests
+{
+    public class PageInfoTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NextPageNumberTests()
+        {
+        }
+
+        [Test]
+        public void PreviousPageNumberTests()
+        {
+        }
+
+        [Test]
+        public void SelectPageNumberTests()
+        {
+        }
+    }
+}
