659648b523575b430669c747bc02b9e050294929
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-29 01:05:30 +0300

    Update for variable and assets

--- PaQuery/Extensions/HttpRequestExtensions.cs
+++ PaQuery/Extensions/HttpRequestExtensions.cs
@@ -15,15 +15,15 @@ namespace PaQuery.Extensions
             string queryString = request.Query.ToStringQueriesWithoutPageQueryKey(pageQueryKey);
             string hostPath = request.IsHttps ? $"https://{request.Host}{request.Path}" : $"https://{request.Host}{request.Path}";
 
-            for (sbyte i = (sbyte)PageType.Previous; i <= (sbyte)PageType.Next; i += 2)
+            for (PageType i = PageType.Previous; i <= PageType.Next; i += 2)
             {
-                int? pageNumber = Models.PaginationUrl.SelectPageNumber((PageType)i, totalPageCount, currentPage);
+                int? pageNumber = Models.PaginationUrl.SelectPageNumber(i, totalPageCount, currentPage);
                 if (pageNumber == null)
                 {
                     continue;
                 }
 
-                pagination.SetUrls((PageType)i, queryString, pageQueryKey, hostPath, (int)pageNumber);
+                pagination.SetUrls(i, queryString, pageQueryKey, hostPath, (int)pageNumber);
             }
             return pagination;
         }
